07125332f2333a575ea1b236d56c07e2d80a18e3
MorganCartwright2022 <morganjcartwright@gmail.com>
2022-07-23 11:09:54 -0500

    Added start comand

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
