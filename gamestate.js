9b147583d2c42064c91f14d3054974d50dd29b8c
MorganCartwright2022 <morganjcartwright@gmail.com>
2022-07-31 10:58:19 -0500

    Added gameboard

---
+++ gamestate.js
@@ -0,0 +1,23 @@
+const gamestate = {
+    name: "game",
+    boardSize: {rows: 0, cols: 0},
+    chat: [],
+    icons: [],
+    colors: [],
+    chat: (text) => gamestate.chat.push(text),
+    addIcon: (x, y, src) => gamestate.icons.push({x, y, src}),
+    removeIcon: (x, y) => gamestate.icons = gamestate.icons.filter(icon => icon.x !== x || icon.y !== y),
+    newBoard: (rows, cols) => gamestate.boardSize = {rows, cols},
+    addColor: (x, y, hex) => gamestate.colors.push({x, y, hex}),
+    toJson: () => {
+        return {
+            "name": gamestate.name,
+            "boardSize": gamestate.boardSize,
+            "chat": gamestate.chat,
+            "icons": gamestate.icons,
+            "colors": gamestate.colors
+        }
+    }
+}
+
+exports.gamestate = gamestate;
--- server.js
+++ server.js
@@ -1,21 +1,87 @@
 const express = require("express");
 const http = require("http");
+const { receiveMessageOnPort } = require("worker_threads");
 const WebSocket = require("ws");
+const gamestate = require("./gamestate.js").gamestate;
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(express);
 const wss = new WebSocket.Server({server});
 
+/*
+ * Handle incoming messages
+ */
+function receiveMessage(message, client) {
+  // Get the message data
+  const {type, params} = message;
+
+  // Perform action based on type of message
+  switch(type) {
+      case "chat":
+          gamestate.addChat(params.text);
+          break;
+      case "addIcon":
+          gamestate.addIcon(params.x, params.y, params.src);
+          break;
+      case "removeIcon":
+          gamestate.removeIcon(params.x, params.y);
+          break;
+      case "newBoard":
+          gamestate.newBoard(params.rows, params.cols);
+          break;
+      case "addColor":
+          gamestate.addColor(params.x, params.y, params.hex);
+          break;
+      case "ping":
+          break;
+      case "newClient":
+        const message = {
+            type: "serverData",
+            params: {
+              gamestate: gamestate.toJson()
+            }
+        }
+        client.send(JSON.stringify(message));
+        break;
+      default:
+          console.log("Unexpected message type: " + type);
+  }
+}
+
+/*
+ * Set up web socket interactions
+ */
 wss.on('connection', ws => {
+  // On connecting, look for messages
   ws.on('message', data => {
-    wss.clients.forEach(client => {
-      if(client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data.toString());
-      }
-    });
+    // On receiving a message, convert it to a Java object
+    const message = JSON.parse(data.toString());
+
+    // If this is not a server message, send it to the other clients
+    if(message.type != "newClient") {
+      wss.clients.forEach(client => {
+        if(client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data.toString());
+        }
+      });
+    }
+    
+    receiveMessage(message, ws);
   });
 });
+
+setInterval(() => {
+  wss.clients.forEach(client => {
+    const message = {
+      type: "ping",
+      params: {
+        time: (new Date()).getTime
+      }
+    };
+    client.send(JSON.stringify(message));
+  });
+}, 1000);
 
 server.listen(port, () => {
   console.log("Listening on port: " + port);
