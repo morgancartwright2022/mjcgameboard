95e84fb151869a47c27a567ac18bab8ae505691c
MorganCartwright2022 <morganjcartwright@gmail.com>
2022-08-21 09:57:54 -0500

    Postrges integration and various upgrades

--- gamestate.js
+++ gamestate.js
@@ -1,16 +1,48 @@
 const gamestate = {
     name: "game",
+    id: -1,
     boardSize: {rows: 0, cols: 0},
     chat: [],
     icons: [],
     colors: [],
-    addChat: (text) => {gamestate.chat.push(text)},
-    addIcon: (x, y, src) => gamestate.icons.push({x, y, src}),
-    removeIcon: (x, y) => gamestate.icons = gamestate.icons.filter(icon => icon.x !== x || icon.y !== y),
-    newBoard: (rows, cols) => gamestate.boardSize = {rows, cols},
-    addColor: (x, y, hex) => gamestate.colors.push({x, y, hex}),
+    addChat: (text) => {
+        gamestate.chat.push(text);
+    },
+    removeIcon: (x, y) => {
+        gamestate.icons = gamestate.icons.filter(icon => icon.x !== x || icon.y !== y)
+    },
+    addIcon: (x, y, src) => {
+        gamestate.removeIcon(x, y);
+        gamestate.icons.push({x, y, src});
+    },
+    addIcons: (icons) => {
+        icons.forEach(icon => gamestate.addIcon(icon.x, icon.y, icon.src));
+    },
+    newBoard: (rows, cols) => {
+        gamestate.boardSize = {rows, cols};
+    },
+    removeColor: (x, y) => {
+        gamestate.colors = gamestate.colors.filter(color => color.x !== x || color.y !== y)
+    },
+    addColor: (x, y, hex) => {
+        gamestate.removeColor(x, y);
+        gamestate.colors.push({x, y, hex});
+    },
+    addColors: (colors) => {
+        colors.forEach(icon => gamestate.addColor(icon.x, icon.y, icon.hex))
+    },
+    setId: (id) => {
+        gamestate.id = id
+    },
+    reset: () => {
+        gamestate.chat = [];
+        gamestate.boardSize = {rows: 0, cols: 0};
+        gamestate.icons = [];
+        gamestate.colors = [];
+    },
     toJson: () => {
         return {
+            "id": gamestate.id,
             "name": gamestate.name,
             "boardSize": gamestate.boardSize,
             "chat": gamestate.chat,
--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const wss = new WebSocket.Server({server});
 /*
  * Connect to our database
  */
+
 const dbClient = new Client({
-  connectionString: process.env.DATABASE_URL || local,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
@@ -33,19 +34,99 @@ const sqlUser = {
   }
 }
 
-const sqlTable = {
-  fetchTable: callback => {
-    dbClient.query('SELECT * FROM tables', (err, res) => {
+const sqlBoard = {
+  fetchBoard: (boardId, callback) => {
+    dbClient.query('SELECT * FROM boards WHERE boardId = ' + boardId, (err, res) => {
       if(err) console.log(err);
       else callback(res.rows[0]);
+    });
+  },
+  newBoard: (rows, cols, callback) => {
+    dbClient.query(`INSERT INTO boards (rows, cols) ` + 
+                   `VALUES (${rows}, ${cols})`, (err, res) => {
+      if(err) console.log(err);
+      else {
+        if(callback) {
+          dbClient.query("SELECT currval(pg_get_serial_sequence('boards', 'boardid'))", (err, res) => {
+            if(err) console.log(err);
+            else {
+              callback(res.rows[0].currval);
+            }
+          });
+        };
+      }
+    });
+  }
+}
+
+const sqlIcon = {
+  fetchIcons: (boardId, callback) => {
+    dbClient.query('SELECT * FROM icons WHERE boardId = ' + boardId, (err, res) => {
+      if(err) console.log(err);
+      else callback(res.rows);
+    });
+  },
+  saveIcons: (boardId, icons, callback) => {
+    dbClient.query('DELETE FROM icons WHERE boardId = ' + boardId, (err) => {
+      if(err) console.log(err);
+      else {
+        let success = true;
+        icons.forEach(icon => {
+          dbClient.query(`INSERT INTO icons (boardId, x, y, src) ` +
+                         `VALUES (${boardId}, ${icon.x}, ${icon.y}, '${icon.src}')`, (err) => {
+            if(err) {
+              console.log(err);
+              success = false;
+            }
+          });
+        });
+        if(callback)
+          callback(success);
+      }
+    });
+  }
+}
+
+const sqlColor = {
+  fetchColors: (boardId, callback) => {
+    dbClient.query("SELECT * FROM colors WHERE boardId = " + boardId, (err, res) => {
+      if(err) console.log(err);
+      else callback(res.rows);
+    });
+  },
+  saveColors: (boardId, colors, callback) => {
+    dbClient.query('DELETE FROM colors WHERE boardId = ' + boardId, (err) => {
+      if(err) console.log(err);
+      else {
+        let success = true;
+        colors.forEach(color => {
+          dbClient.query(`INSERT INTO colors (boardId, x, y, hex) ` +
+                         `VALUES (${boardId}, ${color.x}, ${color.y}, '${color.hex}')`, (err) => {
+            if(err) {
+              console.log(err);
+              success = false;
+            }
+          });
+        });
+        if(callback)
+          callback(success);
+      }
     });
   }
 }
 
 /*
+ * Save a board
+ */
+function saveBoard() {
+  sqlIcon.saveIcons(gamestate.id, gamestate.icons);
+  sqlColor.saveColors(gamestate.id, gamestate.colors);
+}
+
+/*
  * Handle incoming messages
  */
-function receiveMessage(message, client) {
+function receiveMessage(message, sender, allClients) {
   // Get the message data
   const {type, params} = message;
 
@@ -61,19 +142,50 @@ function receiveMessage(message, client) {
           gamestate.removeIcon(params.x, params.y);
           break;
       case "newBoard":
-          gamestate.newBoard(params.rows, params.cols);
+          gamestate.reset();
+          sqlBoard.newBoard(params.rows, params.cols, (boardId) => {
+            gamestate.newBoard(params.rows, params.cols);
+            gamestate.setId(boardId);
+            sendMessage(sender, "chat", {text: "Server: New board created, ID = " + boardId + "."});
+            allClients.forEach(client => {
+              sendMessage(client, "newBoard", {boardId: boardId, rows: params.rows, cols: params.cols});
+            });
+          });
           break;
       case "addColor":
           gamestate.addColor(params.x, params.y, params.hex);
           break;
       case "ping":
           break;
-      case "newClient":
-        sqlTable.fetchTable(table => {
-          sendMessage(client, "serverData", {
-            gamestate: gamestate.toJson(),
-            table: table
+      case "loadBoard":
+        // Fetch everything
+        const boardId = params.boardId;
+        sqlBoard.fetchBoard(boardId, board => {
+          sqlIcon.fetchIcons(boardId, icons => {
+            sqlColor.fetchColors(boardId, colors => {
+              // Add to board
+              gamestate.reset();
+              gamestate.newBoard(board.rows, board.cols);
+              gamestate.addIcons(icons);
+              gamestate.addColors(colors);
+              gamestate.setId(boardId);
+
+              allClients.forEach(client => {
+                sendMessage(client, "serverData", {
+                  gamestate: gamestate.toJson(),
+                });
+              })
+            });
           });
+        });
+        break;
+      case "saveBoard":
+        // Save everything
+        saveBoard();
+        break;
+      case "newClient":
+        sendMessage(sender, "serverData", {
+          gamestate: gamestate.toJson(),
         });
         break;
       default:
@@ -98,8 +210,11 @@ wss.on('connection', ws => {
     // On receiving a message, convert it to a Java object
     const message = JSON.parse(data.toString());
 
+    // Set up list of messages that don't redirect to client
+    const serverMessageTypes = ["newClient", "ping", "newBoard", "loadBoard", "saveBoard"];
+
     // If this is not a server message, send it to the other clients
-    if(message.type != "newClient" && message.type != "ping") {
+    if(!serverMessageTypes.includes(message.type)) {
       wss.clients.forEach(client => {
         if(client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(data.toString());
@@ -107,7 +222,7 @@ wss.on('connection', ws => {
       });
     }
     
-    receiveMessage(message, ws);
+    receiveMessage(message, ws, wss.clients);
   });
 });
 
@@ -122,6 +237,8 @@ setInterval(() => {
     client.send(JSON.stringify(message));
   });
 }, 1000);
+
+setInterval(() => saveBoard(), 10000);
 
 server.listen(port, () => {
   console.log("Listening on port: " + port);
